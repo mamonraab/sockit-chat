1821fe495561ddbfc36ee22f6f81bec2436ce83c
mamon rasool <mamon@mamonrasool.com>
2016-11-06 00:26:28 +0300

    doing init socket

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-
+//begin socket work
+var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/www'));
 
+io.on('connection', function() {
+    console.log('user conacted via sockt.io');
+});
 http.listen(PORT, function() {
     console.log('server start at port ' + PORT);
 });
