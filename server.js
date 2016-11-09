23bb53c565aa0fefe6472fef3bf85683c2913296
mamon rasool <mamon@mamonrasool.com>
2016-11-09 12:53:52 +0300

    complate th ui

--- server.js
+++ server.js
@@ -2,12 +2,35 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var xssFilters = require('xss-filters');
 //begin socket work
 var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/www'));
 
-io.on('connection', function() {
+io.on('connection', function(socket) {
     console.log('user conacted via sockt.io');
+
+    // send msg to all exept the sender
+    socket.on('eventname', function(data) {
+        data.text = xssFilters.inHTMLData(data.text);
+        console.log(data.text);
+
+
+        //send to all
+        io.emit('eventname', data);
+
+
+        //send data to all clinet except sender
+        //   socket.broadcast.emit('eventname', data);
+
+    });
+
+    //send data to clinet
+
+    socket.emit('eventname', {
+        text: "hey these is my chat "
+    });
+
 });
 http.listen(PORT, function() {
     console.log('server start at port ' + PORT);
--- www/js/app.js
+++ www/js/app.js
@@ -3,3 +3,21 @@ var socket = io();
 socket.on('connect', function() {
     alert('conacteds !!!!!');
 });
+
+socket.on('eventname', function(data) {
+    console.log(data.text);
+
+    jQuery('.msgs').append('<p>' + data.text + '</p>')
+});
+
+//handling form submit and msg
+var $Form = jQuery('#message-form');
+
+$Form.on('submit', function(evnt) {
+
+    evnt.preventDefault();
+    socket.emit('eventname', {
+        text: $Form.find('input[name=msg]').val()
+    });
+    $Form.find('input[name=msg]').val('');
+});
